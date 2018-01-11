ee324db694bc433b62e91a381653c8f8cbb5f30b
Thomas Laird <t6r6l5@gmail.com>
2018-01-11 03:29:48 -0700

    basic header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+import { NavLink } from 'react-router-dom';
+
+const Header = (props) => {
+
+  return (
+    <header>
+      <h1>
+        <span>Thomas Laird</span>
+        <span>Software Developer</span>
+      </h1>
+      <nav>
+        <ul>
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/projects">Projects</NavLink></li>
+          <li><NavLink to="/blogs">Blogs</NavLink></li>
+          <li><a href="https://github.com/t-laird" target="_blank" rel="noopener noreferrer">GitHub<i className="icon-link-ext"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/thomasrlaird/" target="_blank" rel="noopener noreferrer">LinkedIn<i className="icon-link-ext"></i></a></li>
+          <li><a href="https://www.turing.io/sites/default/files/resumes/ThomasLaird.pdf" target="_blank" rel="noopener noreferrer">Resume<i className="icon-link-ext"></i></a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
