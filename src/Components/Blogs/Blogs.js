f08e67adc5af2d2b2180b83b56f20e3d8f0875b7
Thomas Laird <t6r6l5@gmail.com>
2018-01-21 21:52:04 -0700

    remove CL

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './Blogs.css';
 
 const Blogs = (props) => {
-  console.log('rendering blogs');
-
   return (
     <div className="Blogs">
       <h1><i className="icon-traffic-cone"></i>Under Construction<i className="icon-traffic-cone"></i></h1>
     </div>
   );
-}
+};
 
 export default Blogs;
