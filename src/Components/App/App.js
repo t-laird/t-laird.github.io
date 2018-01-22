d371294a950a83b90058c5da1cf3151ce189bbb1
Thomas Laird <t6r6l5@gmail.com>
2018-01-21 21:51:22 -0700

    create route component for route handling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
-import { Switch, Route } from 'react-router';
-import Blogs from '../Blogs/Blogs';
-import Home from '../Home/Home';
-import Projects from '../Projects/Projects';
+import Routes from '../Routes/Routes';
+import { Route } from 'react-router';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <main>
-          <Switch>
-            <Route path="/projects" component={Projects} />
-            <Route path="/blogs" component={Blogs} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </main>
+        <Route component={Routes} />
       </div>
     );
   }
