e8e1c31d0c1f1241648e61e86d7e8f86b81f1431
Thomas Laird <t6r6l5@gmail.com>
2018-02-19 19:44:03 -0700

    update blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,7 +4,14 @@ import './Blogs.css';
 const Blogs = (props) => {
   return (
     <div className="Blogs">
-      <h1><i className="icon-traffic-cone"></i>Under Construction<i className="icon-traffic-cone"></i></h1>
+      <h1><a 
+        rel="noopener noreferrer" 
+        href="https://medium.com/@thomaslaird/diving-in-to-open-source-d2f5ecd20210" 
+        target="_blank">Diving in to Open Source <i className="icon-link-ext"></i></a></h1>
+      <h1><a 
+        rel="noopener noreferrer" 
+        href="https://medium.com/@thomaslaird/multiple-react-router-app-deployments-on-github-pages-rafrex-spa-f8f9de04dacf" 
+        target="_blank">Multiple React/Router App Deployments on Github Pages — Rafrex SPA <i className="icon-link-ext"></i></a></h1>
     </div>
   );
 };
