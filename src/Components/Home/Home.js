864bba82acf5ec6087aca46ea2b67c691046416a
Thomas Laird <t6r6l5@gmail.com>
2018-03-25 19:42:04 -0600

    change hp language

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Home = (props) => {
   return (
     <div className="Home">
-      <h1>Looking for another member for your team?</h1>
-      <h4>I'm a developer looking for an opportunity where I can contribute meaningfully to an already awesome product or service!</h4>
+      <h1>Hi, I'm Thomas!</h1>
+      <h4>I'm a software developer with a passion for building things that people love to use. Please check out some of my  <Link to="/projects">previous projects</Link> or <a href="mailto:lairdthomasr@gmail.com">drop me a line</a> if you have something that needs more hands to be built!</h4>
       <div className="linkContainer">
         <Link to="/projects">Check Out My Projects</Link>
         <a href="https://www.turing.io/sites/default/files/resumes/ThomasLaird-Resume.pdf" target="_blank" rel="noopener noreferrer">Look at my Resume<i className="icon-link-ext"></i></a>
