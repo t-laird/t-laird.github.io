d013a6980483531f2813720243df699833ed068a
Thomas Laird <t6r6l5@gmail.com>
2020-02-05 08:03:04 -0700

    fix res link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = (props) => {
           <li><NavLink to="/blogs">Blogs</NavLink></li>
           <li><a href="https://github.com/t-laird" target="_blank" rel="noopener noreferrer">GitHub<i className="icon-link-ext"></i></a></li>
           <li><a href="https://www.linkedin.com/in/thomasrlaird/" target="_blank" rel="noopener noreferrer">LinkedIn<i className="icon-link-ext"></i></a></li>
-          <li><a href="https://www.turing.io/sites/default/files/resumes/ThomasLaird-Resume.pdf" target="_blank" rel="noopener noreferrer">Resume<i className="icon-link-ext"></i></a></li>
+          <li><a href={process.env.PUBLIC_URL + '/tl-resume.pdf'} target="_blank" rel="noopener noreferrer">Resume<i className="icon-link-ext"></i></a></li>
         </ul>
       </nav>
     </header>
